78bf6fcb76180051818e282b37cec2cb5d50808c
William Casarin <jb55@jb55.com>
2017-11-27 17:01:40 -0800

    updates

--- netscan.sh
+++ netscan.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 INTERFACE=${1:-$(ifconfig | grep ^e | cut -d":" -f1 | head -n1)}
 echo "using interface $INTERFACE"
 IPRANGE=$(ifconfig $INTERFACE | grep inet\ | cut -d" " -f6 | cut -d"." -f1-3)".1-255"
